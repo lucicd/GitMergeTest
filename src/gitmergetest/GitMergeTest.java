3ffd323f986f023f2a5fbfc4e5a19e87969e8699
lucicd <lucicd@hotmail.com>
2018-11-28 11:11:51 +0400

    Adding sayGoodbye() method into Greeter class.

---
+++ src/gitmergetest/GitMergeTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitmergetest;
+
+import view.Greeter;
+
+/**
+ *
+ * @author Drazen
+ */
+public class GitMergeTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Greeter.sayHello();
+        Greeter.sayGoodbye();
+    }
+}
---
+++ src/view/Greeter.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Drazen
+ */
+public class Greeter {
+    public static void sayHello() {
+        System.out.println("Hello world!");
+    }
+    
+    public static void sayGoodbye() {
+        System.out.println("Goodbye world!");
+    }
+}
